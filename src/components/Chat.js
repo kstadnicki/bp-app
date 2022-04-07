7788ff0f790a9158d53b6a28a542746c754bd066
PrzemyslawTrabuc <czemek17@gmail.com>
2022-04-07 16:56:59 +0200

    dodałem pobieranie wiadomości jako tako, jeszcze się same nie odświeżają

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import db from './firebase/firebase.js';
+import React, {useState, useEffect} from 'react';
+import db from '../firebase/firebase';
 import { getDocs, collection, doc, setDoc, addDoc, query, orderBy, limit, getDoc } from 'firebase/firestore';
 
 const Chat = ({imie, rola, userid})=>{
+
+    const [Messages,setMessages]=useState([])
+    const [messagesCount,setMessagesCount]=useState([])
 
     const onLoginChange =(event)=>{
         //setLogin(event.target.value);
@@ -12,10 +15,50 @@ const Chat = ({imie, rola, userid})=>{
         event.preventDefault();
 
     }
-     
-    const response = getDoc(doc(db,"msg","1"));
 
-    console.log(response);
+    const getMsgs =async()=>{
+        const response = await getDocs(collection(db,"msg"));
+        setMessagesCount(response.docs.length);
+        setMessages(response);
+    }
+
+    const listOfMsgs=()=>{
+        let MSGS =[];     
+            Messages.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots                     
+                MSGS.push(
+                    <li className="card" key={doc.id}>
+                        <h5>{doc.data().id}:</h5> 
+                        {doc.data().text}                    
+                    </li>
+                    
+                ); 
+                console.log(doc.id +" -> "+ doc.data().text)              
+            });
+          return(
+          <div className="container-lg">   
+            <div className="card">
+                <div className="card-header">
+                    <h3 className="text-center">Najlepszy czatek bulwo</h3>
+                </div>
+                    <ul className="list-group list-group-flush">
+                    {MSGS}
+                    </ul>
+            </div>
+        </div> 
+        )
+           
+    }
+
+    useEffect(() =>{  
+      getMsgs();    
+      //console.log(messagesCount); 
+    },[messagesCount])
+
+     
+    //const response = getDoc(doc(db,"msg","1"));
+
+    
     return (
         <div>
             {/* <div>
@@ -24,7 +67,7 @@ const Chat = ({imie, rola, userid})=>{
             }
             </div > */}
             <div id="messenger">
-
+            {listOfMsgs()}
             </div>
 
             <form onSubmit={onFormSubmit}>
