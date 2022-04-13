410783654c8a4d79f42e6a0d11272b09ad0100c0
StadnickiK <stakrzysztof@interia.pl>
2022-04-13 20:05:09 +0200

    Create Calendar base component

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Chat from './Chat';
+import Calendar from './Calendar/Calendar.js';
 
 const WelcomeScreen = ({imie, rola, userid})=>{
     return(
         <div>
             <h1 className="float-md-end">Witaj {imie} Rola: {rola}</h1>
-            {/* <Chat imie={imie} rola={rola} userid = {userid}></Chat>               */}
+            <Calendar />              
         </div>
     );
 }
